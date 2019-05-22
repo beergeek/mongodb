14d2ceff5020ed4d0ab1ce3a13447450497e1ce0
beergeek <beergeek1679@gmail.com>
2019-05-23 08:37:28 +1000

    New project task

---
+++ tasks/deploy_instance.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+OPS_MANAGER_URL=PT_ops_manager_url
+PROJECT_ID=PT_project_id
+CA_CERT_PATH=PT_curl_ca_cert_path
+JSON_DATA=PT_json_payload
+USERNAME=PT_curl_username
+TOKEN=PT_curl_token
+
+if [ $CA_CERT_PATH ]:
+  CA_OPTION="--cacert ${CA_CERT_PATH}"
+fi
+
+curl -u ${USERNAME}:${TOKEN} $CA_OPTION -X PUT  -H "Content-Type: application/json" "${OPS_MANAGER_URL}/api/public/v1.0/groups/${PROJECT_ID}/automationConfig?pretty=true" --digest  -d \'${JSON_DATA}\'
---
+++ tasks/make_project.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+OPS_MANAGER_URL=PT_ops_manager_url
+PROJECT_NAME=PT_project_name
+ORG_ID=PT_org_id
+CA_CERT_PATH=PT_curl_ca_cert_path
+USERNAME=PT_curl_username
+TOKEN=PT_curl_token
+
+if [ $CA_CERT_PATH ]:
+  CA_OPTION="--cacert ${CA_CERT_PATH}"
+fi
+
+curl -u ${USERNAME}:${TOKEN} $CA_OPTION -X PUT  -H "Content-Type: application/json" "${OPS_MANAGER_URL}/api/public/v1.0/groups/?pretty=true"--digest  -d \'{"name": "${PROJECT_NAME"}, "orgId": "${ORG_ID}"}\'
