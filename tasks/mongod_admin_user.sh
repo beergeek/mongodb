dcbc5971ba24b6abe4979974e5901728d11246c2
beergeek <beergeek1679@gmail.com>
2019-08-16 10:21:39 +1000

    Smarter creation of user

--- tasks/mongod_admin_user.sh
+++ tasks/mongod_admin_user.sh
@@ -5,11 +5,12 @@ passwd=$PT_passwd
 port=$PT_port
 x509_path=$PT_x509_path
 ca_path=$PT_ca_path
+replica_set=$PT_replica_set
 
 if [ ! -z "${x509_path}" ] && [ "${x509_path}" != 'null' ]; then
-echo "db.createUser( { 'user': '${user}', 'pwd': '${passwd}', roles: [ { 'db': 'admin', 'role': 'root'} ] } )"
-mongo admin --port $port --ssl --sslAllowInvalidHostnames --sslPEMKeyFile ${x509_path} --sslCAFile ${ca_path} --eval "db.createUser( { 'user': '${user}', 'pwd': '${passwd}', roles: [ { 'db': 'admin', 'role': 'root'} ] } )"
+  echo "db.createUser( { 'user': '${user}', 'pwd': '${passwd}', roles: [ { 'db': 'admin', 'role': 'root'} ] } )"
+  mongo admin mongodb://127.0.0.1:${port}?replicaSet=${replica_set} --ssl --sslAllowInvalidHostnames --sslPEMKeyFile ${x509_path} --sslCAFile ${ca_path} --eval "db.createUser( { 'user': '${user}', 'pwd': '${passwd}', roles: [ { 'db': 'admin', 'role': 'root'} ] } )"
 else
-mongo admin --port $port --eval "db.createUser( { 'user': '${user}', 'pwd': '${passwd}', roles: [ { 'db': 'admin', 'role': 'root'} ] } )"
+  mongo admin mongodb://127.0.0.1:${port}?replicaSet=${replica_set} --eval "db.createUser( { 'user': '${user}', 'pwd': '${passwd}', roles: [ { 'db': 'admin', 'role': 'root'} ] } )"
 fi
 exit 0
