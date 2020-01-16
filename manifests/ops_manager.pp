5b8c530793081fbfeeac5f7c789f51924475e7cf
beergeek <beergeek1679@gmail.com>
2020-01-17 09:37:23 +1100

    LDAP for OM

--- manifests/ops_manager.pp
+++ manifests/ops_manager.pp
@@ -158,14 +158,14 @@ class mongodb::ops_manager (
   }
 
   if $auth_type == 'com.xgen.svc.mms.svc.user.UserSvcLdap' and
-  ($ldap_bind_dn == undef or $ldap_bind_url_port == undef or
-  $ldap_bind_password == undef or $ldap_global_role_owner == undef or
+  ($ldap_bind_dn == undef or $ldap_url_port == undef or
+  $ldap_bind_password == undef or $ldap_global_owner == undef or
   $ldap_url_host == undef or $ldap_user_group == undef or
   $ldap_user_search_attribute == undef) {
     fail("If LDAP auth is enabled for Ops Manager the following must be provided:\n\t
-    * ldap_bindDn_host\n\t* ldap_bindDn_port\n\t* ldap_bindPassword\n\t
-    * ldap_global_role_owner\n\t* ldap_url\n\t* ldap_user_group\n\t
-    * ldap_user_searchAttribute")
+    * ldap_bind_dn\n\t* ldap_url_port\n\t* ldap_bind_password\n\t
+    * ldap_global_owner\n\t* ldap_url_host\n\t* ldap_user_group\n\t
+    * ldap_user_search_attribute")
   }
 
   if $enable_http_service {
