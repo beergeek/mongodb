829fdda68c125ad205cf34340703e1818861165e
beergeek <beergeek1679@gmail.com>
2020-02-24 15:33:13 +1100

    Remove unneeded

--- lib/puppet/util/network_device/mongodb_om/facts.rb
+++ lib/puppet/util/network_device/mongodb_om/facts.rb
@@ -12,7 +12,7 @@ class Puppet::Util::NetworkDevice::Mongodb_om::Facts
     }
 
     if response = @transport.call('/api/public/v1.0')
-      debug(response)
+      Puppet.debug response
       result = response
     else
       Puppet.warning("Did not receive device details. REST requires token access and whitelisting.")
