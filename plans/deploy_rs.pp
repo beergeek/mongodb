f34a85559aca89ebbf18ab1e1ed8f3ebcf6ec663
beergeek <beergeek1679@gmail.com>
2020-03-04 16:20:31 +1300

    New plan for projects

--- plans/deploy_rs.pp
+++ plans/deploy_rs.pp
@@ -11,24 +11,24 @@ plan mongodb::deploy_rs (
       Optional[slave_delay]   => Integer[0],
       Optional[vote]          => Integer[0],
     }]
-  ]                   $replica_set_members,
-  String[24]          $project_id,
-  String[1]           $curl_token,
-  String[1]           $curl_username,
-  String[1]           $ops_manager_url,
-  String[1]           $replica_set_name,
-  Enum['none','preferSSL','requireSSL'] $ssl_mode                    = 'preferSSL',
-  Enum['none','x509','keyFile']         $cluster_auth_type           = 'x509',
-  Boolean                               $enable_encryption           = false,
-  Optional[String[1]] $curl_ca_file_path       = undef,
-  Boolean             $enable_kerberos         = true,
-  Optional[String[1]] $encryption_keyfile_path = undef,
-  Optional[String[1]] $keytab_file_path        = undef,
-  Optional[String[1]] $client_certificate_path = undef,
-  String[1]           $db_path                 = '/data/db',
-  String[1]           $log_file_path           = '/data/logs/mongodb.log',
-  String[1]           $mongodb_version         = '4.2.3-ent',
-  String[1]           $mongodb_compat_version  = '4.2',
+  ]                                     $replica_set_members,
+  String[24]                            $project_id,
+  String[1]                             $curl_token,
+  String[1]                             $curl_username,
+  String[1]                             $ops_manager_url,
+  String[1]                             $replica_set_name,
+  Boolean                               $enable_encryption       = false,
+  Boolean                               $enable_kerberos         = true,
+  Enum['none','preferSSL','requireSSL'] $ssl_mode                = 'preferSSL',
+  Enum['none','x509','keyFile']         $cluster_auth_type       = 'x509',
+  Optional[String[1]]                   $client_certificate_path = undef,
+  Optional[String[1]]                   $curl_ca_file_path       = undef,
+  Optional[String[1]]                   $encryption_keyfile_path = undef,
+  Optional[String[1]]                   $keytab_file_path        = undef,
+  String[1]                             $db_path                 = '/data/db',
+  String[1]                             $log_file_path           = '/data/logs/mongodb.log',
+  String[1]                             $mongodb_compat_version  = '4.2',
+  String[1]                             $mongodb_version         = '4.2.3-ent',
 ) {
 
   $_defaults = {
@@ -49,16 +49,9 @@ plan mongodb::deploy_rs (
     project_id        => $project_id,
   })
 
-  #$current_config = $current_state.first.value.reduce({}) |$current, $value| {
-  #  if ($value[0] != 'processes') and ($value[0] != 'replicaSets') {
-  #    $current + {$value[0] => $value[1]}
-  #  } else {
-  #    $current
-  #  }
-  #}
-
   # we have to merge the defaults into each hash of the array.....
   # Try not to let your brain explode!
+  # Create the processes hash
   $_replica_set_members = $replica_set_members.reduce({}) |$k, $v| {
     $member_data = merge($_defaults + $v[1])
     $net_and_rep = {
@@ -140,20 +133,26 @@ plan mongodb::deploy_rs (
     $k + {$v[0] => {'args2_6' => merge($net_and_rep, $security, $storage_and_log, $krb5)}}
   }
 
-  #$new_config = $current_state.first.value + Hash(epp('mongodb/new_rs.epp', {
-  #  replica_set_members => $_replica_set_members,
-  #  ssl_mode            => $ssl_mode,
-  #  replica_set_name    => $replica_set_name,
-  #  cluster_auth_type   => $cluster_auth_type,
-  #  enable_encryption   => $enable_encryption,
-  #  encryption_keyfile_path => $encryption_keyfile_path,
-  #  db_path                 => $db_path,
-  #  log_file_path           => $log_file_path,
-  #  mongodb_version         => $mongodb_version,
-  #  mongodb_compat_version  => $mongodb_compat_version,
-  #  enable_kerberos         => $enable_kerberos,
-  #  keytab_file_path        => $keytab_file_path,
-  #}))
+  # Create the replicaSets hash
+  $replica_sets_data = $replica_set_members.reduce({}) |$k, $member_data| {
+    $k + {
+      '_id'          => $member_data[1]['id'],
+      'arbiterOnly'  => $member_data[1]['arbitor'],
+      'buildIndexes' => $member_data[1]['build_indexes'],
+      'hidden'       => $member_data[1]['hidden'],
+      'host'         => $member_fqdn[0],
+      'priority'     => $member_data[1]['priority'],
+      'slaveDelay'   => $member_data[1]['slave_delay'],
+      'votes'        => $member_data[1]['vote']
+    }
+  }
 
-  return $_replica_set_members
+  $replica_sets =  {
+    '_id'             => $replica_set_name,
+    'members'         => [$replica_sets_data],
+    'protocolVersion' => '1',
+    'settings'        => { },
+  }
+
+  return merge($current_state.first.value, {'processes' => [$_replica_set_members]}, {'replicaSets' => [$replica_sets]})
 }
